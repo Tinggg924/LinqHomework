2e0ef99f76066ee44e2d1e55ad8ea942ee29248a
Tinggg924 <162138482+Tinggg924@users.noreply.github.com>
2024-09-18 00:25:30 +0800

    Add files via upload

---
+++ LinqLabs/2. FrmLangForLINQ.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace Starter
+{
+
+    //Notes: LINQ 主要參考 
+    //組件 System.Core.dll,
+    //namespace {}  System.Linq
+    //public static class Enumerable
+   
+    //public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate);
+
+    //1. 泛型 (泛用方法)                                          (ex.  void SwapAnyType<T>(ref T a, ref T b)
+    //2. 委派參數 Lambda Expression (匿名方法簡潔版)               (ex.  MyWhere(nums, n => n %2==0);
+    //3. Iterator                                                (ex.  MyIterator)
+    //4. 擴充方法          
+    public partial class FrmLangForLINQ : Form
+    {
+        public FrmLangForLINQ()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //int[] nums = { 1, 2, 3 };
+            //nums.Max()
+
+            int n1, n2;
+            n1 = 100;
+            n2 = 200;
+
+            MessageBox.Show(n1 + "," + n2);
+
+          
+            Swap(ref n1, ref n2);  //FrmLangForLINQ.Swap(ref n1, ref n2);
+            //MessageBox.Show(SystemInformation.ComputerName);
+            //Application.StartupPath
+
+            MessageBox.Show(n1 + "," + n2);
+
+            //==============================
+
+            string s1 = "aaa", s2 = "bbb";
+            MessageBox.Show(s1 + "," + s2);
+
+            Swap(ref s1, ref s2);
+            MessageBox.Show(s1 + "," + s2);
+
+        }
+
+        private static void Swap(ref int n1, ref int n2)
+        {
+            int temp = n2;
+            n2 = n1;
+            n1 = temp;
+        }
+
+        private static void Swap(ref string n1, ref string n2)
+        {
+            string temp = n2;
+            n2 = n1;
+            n1 = temp;
+        }
+
+        //..........
+        private static void Swapxxx(ref object n1, ref object n2)
+        {
+            object temp = n2;
+            n2 = n1;
+            n1 = temp;
+        }
+
+        private static void SwapAnyType<T>(ref T n1, ref T n2)
+        {
+            T temp = n2;
+            n2 = n1;
+            n1 = temp;
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int n1, n2;
+            n1 = 100;
+            n2 = 200;
+            MessageBox.Show(n1 + "," + n2);
+
+            SwapAnyType<int>(ref n1, ref n2);
+            MessageBox.Show(n1 + "," + n2);
+            //==============================
+            string s1 = "aaa", s2 = "bbb";
+            MessageBox.Show(s1 + "," + s2);
+
+            // SwapAnyType<string>(ref s1, ref s2);
+            SwapAnyType(ref s1, ref s2); //推斷型別
+            MessageBox.Show(s1 + "," + s2);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //具名方法
+            //this.buttonX.Click += new EventHandler( ButtonX_Click);
+            this.buttonX.Click += ButtonX_Click;
+
+            // NOTE:           嚴重性 程式碼 說明 專案  檔案 行   隱藏項目狀態
+            //錯誤(作用中)    CS0123  'aaa' 沒有任何多載符合委派 'EventHandler' LinqLabs C:\Shared\LINQ\LinqLabs(Solution)\LinqLabs\2.FrmLangForLINQ.cs    102
+
+            this.buttonX.Click += aaa;
+
+            //================================
+            //C# 2.0 匿名方法
+            this.buttonX.Click += delegate (object sender1, EventArgs e1)
+                                          {
+                                              MessageBox.Show("匿名方法");
+                                          };
+
+            //===============================
+            //匿名方法 C# 3.0 lambda => goes to
+            this.buttonX.Click += (object sender1, EventArgs e1) =>
+                                    {
+                                        MessageBox.Show("匿名方法 簡潔版");
+                                    };
+
+        }
+
+
+
+        private void ButtonX_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ButtonX click");
+        }
+
+        private void aaa(object sender, EventArgs e)
+        {
+            MessageBox.Show("aaa");
+        }
+
+         bool Test (int n)
+        {
+            return n > 5;
+        }
+
+        bool IsEven(int n)
+        {
+            return n %2==0;
+        }
+        //Step 1: create delegate class
+        //Step 2: create delegate object
+        //Step 3: Invoke method / call method
+
+        delegate bool MyDelegate(int n);
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            bool result;
+            result = Test(3);
+
+          
+            //============================
+            MyDelegate delegateObj  =  new MyDelegate(Test);
+            result =  delegateObj(7);
+
+            //===========================
+
+            delegateObj = IsEven;
+
+            result = delegateObj(7);
+
+
+            MessageBox.Show("result = " + result);
+
+        }
+    }
+}
---
+++ LinqLabs/作業/Frm作業_1.cs
@@ -0,0 +1,182 @@
+﻿using LinqLabs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHomeWork
+{
+    public partial class Frm作業_1 : Form
+    {
+        private static int _currentPage = 0;
+        private static int _pageSize = 0;
+        public Frm作業_1()
+        {
+            InitializeComponent();
+            //NOTE Distinct()
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+            System.IO.FileInfo[] files = dir.GetFiles();
+            this.ordersTableAdapter1.Fill(this.nwDataSet1.Orders);
+            this.order_DetailsTableAdapter1.Fill(this.nwDataSet1.Order_Details);
+            this.productsTableAdapter1.Fill(this.nwDataSet1.Products);
+
+            var pyear = from o in this.nwDataSet1.Orders
+                     select o.OrderDate.Year;
+            this.comboBox1.DataSource = pyear.Distinct().ToList();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            var q = from f in files
+                    where f.Extension == ".log"
+                    select f;
+
+            //files[0].CreationTime
+            this.dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            var p = from c in files
+                    where c.CreationTime.Year == 2024
+                    select c;
+            this.dataGridView1.DataSource = p.ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            var p = from l in files
+                    where l.Length > 100000
+                    select l;
+
+            this.dataGridView1.DataSource = p.ToList();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = nwDataSet1.Orders.ToList();
+            //this.dataGridView2.DataSource = this.nwDataSet1.Order_Details;
+
+            var od = from d in nwDataSet1.Order_Details
+                     join p in nwDataSet1.Products
+                     on d.ProductID equals p.ProductID
+                     select d;
+
+            this.dataGridView2.DataSource = od.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            var d = from p in nwDataSet1.Orders
+                    where p.OrderDate.Year.ToString() == comboBox1.Text
+                    select p;
+
+            this.dataGridView1.DataSource= d.ToList();
+
+            var od = from op in nwDataSet1.Order_Details
+                     join p in nwDataSet1.Orders
+                     on op.OrderID equals p.OrderID
+                     where p.OrderDate.Year.ToString() == comboBox1.Text
+                     select op;
+
+            this.dataGridView2.DataSource= od.ToList();
+
+        }
+
+        private void Frm作業_1_Load(object sender, EventArgs e)
+        {
+            //for (int i = 1990; i <= 2024; i++)
+            //{
+            //    comboBox1.Items.Add(i.ToString());
+            //}
+
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            this.dataGridView2.DataSource = this.nwDataSet1.Products.ToList();
+            lblDetails.Text = "產品列表";
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            int _pageSize = Convert.ToInt32(textBox1.Text);
+
+            if (_currentPage > 0)
+            {
+                // 減少頁數，回到上一頁
+                _currentPage--;
+
+                // 計算需要跳過的記錄數
+                int skip = (_currentPage) * _pageSize;
+
+                // 取得資料，並跳過已顯示的記錄
+                var products = this.nwDataSet1.Products.Skip(skip).Take(_pageSize);
+
+                // 將結果綁定到 DataGridView
+                this.dataGridView2.DataSource = products.ToList();
+            }
+
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+        
+            int _pageSize = Convert.ToInt32(textBox1.Text);
+            int skip = (_currentPage) * _pageSize;
+
+            // 取得資料，並跳過已顯示的記錄
+            var products = this.nwDataSet1.Products.Skip(skip).Take(_pageSize);
+
+            // 將結果綁定到 DataGridView
+            this.dataGridView2.DataSource = products.ToList();
+
+            // 增加頁數，準備下一頁
+            _currentPage++;
+
+
+            //Distinct()
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectId = (int)dataGridView1.CurrentRow.Cells["OrderID"].Value;
+
+            var selectedOrder = from n in nwDataSet1.Order_Details
+                                where n.OrderID == selectId
+                                select n;
+
+            dataGridView2.DataSource = selectedOrder.ToList();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var eachYOrder = from n in nwDataSet1.Orders
+                             group n by n.OrderDate.Year into y
+                             select new { Year = y.Key, Amount = y.Count() };
+            dataGridView2.DataSource = eachYOrder.ToList();
+        }
+    }        
+}
